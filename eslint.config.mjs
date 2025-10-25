22ad34b7600dc756ac66dfeca6fe39975c197ee4
lil-shimon <shimon101018@gmail.com>
2025-10-25 14:29:47 +0900

    Fix ESLint circular structure error with Next.js 16
    Remove FlatCompat usage and import eslint-config-next/core-web-vitals directly.
    This resolves the 'Converting circular structure to JSON' error that occurs
    when using FlatCompat with Next.js 16 and ESLint 9.
    
    Refs: https://github.com/vercel/next.js/issues/85244

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,9 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
 import storybook from "eslint-plugin-storybook";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -23,6 +13,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...nextVitals,
   ...storybook.configs["flat/recommended"],
 ];
 
